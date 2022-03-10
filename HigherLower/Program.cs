2e6ce4eb77e908aa40c23999bf6383eb73962578
Michal <Michal@MICHAL-KOMPUTER>
2022-03-10 22:13:27 +0100

    Dodaj pliki projektów.

---
+++ HigherLower/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+class Program
+{
+    static void CardType(int Card, int num)
+    {
+        switch (Card)
+        {
+            case 11: Console.WriteLine("Karta " + num + " był to: Jopek."); break;
+            case 12: Console.WriteLine("Karta " + num + " była to: Królowa."); break;
+            case 13: Console.WriteLine("Karta " + num + " był to: Król."); break;
+            default: Console.WriteLine("Karta " + num + " była: " + Card + "."); break;
+        }
+    }
+    static void Main(string[] args)
+    {
+        bool p = true;
+        while (p)
+        {
+            Random r = new Random();
+            int Card1 = r.Next(1, 13);
+            int Card2 = r.Next(1, 13);
+            while (Card1 == Card2) Card2 = r.Next(1, 13);
+            CardType(Card1, 1);
+            Console.WriteLine("Czy Karta 1 jest (W)iększa czy (M)niejsza w porównaniu do karty 2?(domyślnie jest higher):");
+            bool which = true;
+            if (Console.ReadLine() == "M") which = false;
+            CardType(Card2, 2);
+            if (!which && Card2 > Card1 || which && Card1 > Card2) Console.WriteLine("Wygrałeś wpisz end aby zakończyć lub kontynuj.");
+            else Console.WriteLine("Przegrałeś wpisz end aby zakończyć lub kontynuj.");
+            if (Console.ReadLine() == "end") p = false;
+        }
+        Console.WriteLine("GG");
+    }
+}
