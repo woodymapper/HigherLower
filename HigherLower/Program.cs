75086bd1cfe85a5dc112f6d1e274e817c7cfd20b
woodymapper <70817697+woodymapper@users.noreply.github.com>
2022-03-10 22:21:32 +0100

    Update Program.cs

--- HigherLower/Program.cs
+++ HigherLower/Program.cs
@@ -20,14 +20,14 @@ class Program
         {
             Random r = new Random();
             int Card1 = r.Next(1, 13);
-            int Card2 = r.Next(1, 13);
+            int Card2 = r.Next(1, 13);          //randomowość
             while (Card1 == Card2) Card2 = r.Next(1, 13);
             CardType(Card1, 1);
-            Console.WriteLine("Czy Karta 1 jest (W)iększa czy (M)niejsza w porównaniu do karty 2?(domyślnie jest higher):");
-            bool which = true;
+            Console.WriteLine("Czy Karta 1 jest (W)iększa czy (M)niejsza w porównaniu do karty 2?(domyślnie jest higher):"); //pytanie
+            bool which = true; // higher to true lower to false
             if (Console.ReadLine() == "M") which = false;
-            CardType(Card2, 2);
-            if (!which && Card2 > Card1 || which && Card1 > Card2) Console.WriteLine("Wygrałeś wpisz end aby zakończyć lub kontynuj.");
+            CardType(Card2, 2);                                                                                                        
+            if (!which && Card2 > Card1 || which && Card1 > Card2) Console.WriteLine("Wygrałeś wpisz end aby zakończyć lub kontynuj."); //!which to false czyli lower 
             else Console.WriteLine("Przegrałeś wpisz end aby zakończyć lub kontynuj.");
             if (Console.ReadLine() == "end") p = false;
         }
