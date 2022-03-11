cbfb7e193f17af807a31bc6d8cefa3cbdf3fccbc
woodymapper <70817697+woodymapper@users.noreply.github.com>
2022-03-11 18:46:27 +0100

    Update Program.cs

--- HigherLower/Program.cs
+++ HigherLower/Program.cs
@@ -34,7 +34,7 @@ int main()
 
 
 
-
+//Random r = new Random();int Card1 = r.Next(1, 10);main();int main(){int opcja;opcja = Int32.Parse(Console.ReadLine()); if (opcja == Card1){Console.WriteLine("Wygrałeś");if (Card1 < opcja) { Console.WriteLine("Liczba była mniejsza"); }else if (Card1 > opcja) { Console.WriteLine("Liczba była Większa"); } }else if (opcja != Card1){Console.WriteLine("Przegrałeś ");if (Card1 < opcja) { Console.WriteLine("Liczba była mniejsza"); }else if (Card1 > opcja) { Console.WriteLine("Liczba była Większa"); }main();}return Card1;}
 
 
 
